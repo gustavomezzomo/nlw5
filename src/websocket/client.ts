395717061a0c41509377f65c2502bbf597e3a0f4
gustavomezzomo <gkmezzomo@gmail.com>
2021-04-24 08:11:48 -0300

    alterações

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -49,6 +49,10 @@ io.on("connect", (socket) => {
             text,
             user_id
         })
+
+        const allMessages = await messagesService.listByUser(user_id);
+
+        socket.emit("client_list_all_messages", allMessages);
     })
     //salvar a conecxao com socket_id, user_id
 });
